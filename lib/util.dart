7d6925158f38e2c29c6179958d4ea2f4c67aa3b0
Felix1G <felixks110@gmail.com>
2026-01-16 12:37:58 +0700

    Website

--- lib/util.dart
+++
@@ -1,7 +0,0 @@
-String charAtUni(String text, int index) {
-  return String.fromCharCode(text.runes.elementAt(index));
-}
-
-String charAtUniRunes(Runes runes, int index) {
-  return String.fromCharCode(runes.elementAt(index));
-}
